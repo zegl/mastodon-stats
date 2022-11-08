8b5775daed33d169fb08ceeeb416f8b31a8116dc
Gustav Westling <gustav@getsturdy.com>
2022-11-08 15:31:17 +0100

    Add sorting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
 	import data from '$lib/data/servers.json';
 
-	let showServers = data
-		.filter((s) => s.activity[0].statuses > 0 && s.instance.stats.userCount > 10)
-		.sort((a, b) => b.activity[0].statuses - a.activity[0].statuses);
-
 	let sortBy = 'lastWeekStatuses';
+
+	$: showServers = data
+		.filter((s) => s.activity[0].statuses > 0 && s.instance.stats.userCount > 10)
+		.sort((a, b) => {
+			switch (sortBy) {
+				case 'lastWeekStatuses':
+					return b.activity[0].statuses - a.activity[0].statuses;
+				case 'activeUsers':
+					return b.activity[0].logins - a.activity[0].logins;
+				case 'totalUsers':
+					return b.instance.stats.userCount - a.instance.stats.userCount;
+				case 'lastWeekRegistrations':
+					return b.activity[0].registrations - a.activity[0].registrations;
+				case 'lastWeekStatuses':
+					return b.activity[0].statuses - a.activity[0].statuses;
+				case 'totalStatuses':
+					return b.instance.stats.statusCount - a.instance.stats.statusCount;
+			}
+		});
 
 	const setSort = (key: string) => {
 		sortBy = key;
@@ -19,7 +34,10 @@
 
 <p class="text-left">Tracking {showServers.length} active Mastodon servers.</p>
 
-<p class="text-gray-400">By <a class="underline" href="https://mastodon.se/@z">@z@mastodon.se</a> &mdash; Last updated 2022-11-08 &mdash; 🕷</p>
+<p class="text-gray-400">
+	By <a class="underline" href="https://mastodon.se/@z">@z@mastodon.se</a> &mdash; Last updated 2022-11-08
+	&mdash; 🕷
+</p>
 
 <div class="mt-8 flex flex-col">
 	<div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -35,19 +53,51 @@
 								Server
 							</th>
 							<th scope="col" class="whitespace-nowrap  py-3.5 text-left text-sm font-semibold">
-								Users (Active / Total)
+								Users (<span
+									class="cursor-pointer"
+									class:text-red-800={sortBy == 'activeUsers'}
+									on:click={() => (sortBy = 'activeUsers')}
+								>
+									Active
+								</span>
+								/
+								<span
+									class="cursor-pointer"
+									class:text-red-800={sortBy == 'totalUsers'}
+									on:click={() => (sortBy = 'totalUsers')}
+								>
+									Total</span>)
 							</th>
 							<th
 								scope="col"
 								class="whitespace-nowrap  py-3.5 text-left text-sm font-semibold text-gray-900"
 							>
-								Registrations
+								<span
+									class="cursor-pointer"
+									class:text-red-800={sortBy == 'lastWeekRegistrations'}
+									on:click={() => (sortBy = 'lastWeekRegistrations')}
+								>
+									Registrations
+								</span>
 							</th>
 							<th
 								scope="col"
 								class="whitespace-nowrap py-3.5 text-left text-sm font-semibold text-gray-900"
 							>
-								Statuses (Last Week / Total)
+								Statuses (<span
+									class="cursor-pointer"
+									class:text-red-800={sortBy == 'lastWeekStatuses'}
+									on:click={() => (sortBy = 'lastWeekStatuses')}
+								>
+									Last Week
+								</span>
+								/
+								<span
+									class="cursor-pointer"
+									class:text-red-800={sortBy == 'totalStatuses'}
+									on:click={() => (sortBy = 'totalStatuses')}
+								>
+									Total</span>)
 							</th>
 						</tr>
 					</thead>
@@ -55,7 +105,9 @@
 						{#each showServers as server}
 							<tr>
 								<td class="whitespace-nowrap py-2 text-sm text-gray-500 pl-4">
-									<strong><a href={'https://' + server.instance.uri}>{server.instance.uri}</a></strong>
+									<strong
+										><a href={'https://' + server.instance.uri}>{server.instance.uri}</a></strong
+									>
 								</td>
 								<td class="whitespace-nowrap py-2  text-sm text-gray-500 ">
 									{parseInt(server.activity[0].logins).toLocaleString()} / {server.instance.stats.userCount.toLocaleString()}
